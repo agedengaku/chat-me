c377bb4d735a12e5db40afc05897e1cfa417ff7a
JM Taylor <jmotaylor2@gmail.com>
2018-12-03 23:03:12 +0900

    reset textarea and shift+return to submit added

--- src/index.js
+++ src/index.js
@@ -12,26 +12,33 @@ class Wrapper extends React.Component {
 		};
 		this.handleOnChange = this.handleOnChange.bind(this);
 		this.handleSend = this.handleSend.bind(this);
+		this.handleShiftReturn = this.handleShiftReturn.bind(this);
 	}
 	handleOnChange(event) {
 		this.setState({chatInput1: event.target.value});
 	}
-	handleSend() {
-		this.setState({chatOutput1: this.state.chatInput1});
-		this.setState({chatIinput1: ""});
+	handleSend() {		
+		this.setState({chatOutput1: this.state.chatInput1, chatInput1: ""});
+		console.log(this.state.chatInput1);
+	}
+	handleShiftReturn(event) {
+		if (event.keyCode === 13 && event.shiftKey)
+    {
+			this.setState({chatOutput1: this.state.chatInput1, chatInput1: ""});
+			console.log(this.state.chatInput1);    	
+    }
 	}
 	render() {
-		return (<div id='wrapper'><ChatWindow value={this.state.chatOutput1} /><ChatInput onChange={this.handleOnChange} value={this.state.chatInput1} /><SendButton onClick={this.handleSend} /></div>);
+		return (<div id='wrapper'><ChatWindow value={this.state.chatOutput1} /><ChatInput onChange={this.handleOnChange} onKeyUp={this.handleShiftReturn} value={this.state.chatInput1} /><SendButton onClick={this.handleSend} /></div>);
 	}
 }
-
 const ChatWindow = (props) => {
 	const createMarkup = () => { return {__html: props.value}; }	
-	return (<div id={'chat-window'} dangerouslySetInnerHTML={createMarkup()}></div>);
-}
+		return (<div id={'chat-window'} dangerouslySetInnerHTML={createMarkup()}></div>);
+	}
 
 const ChatInput = (props) => {
-	return (<textarea id={'chat-input'} placeholder={'Enter chat here'} onChange={props.onChange} value={props.value}></textarea>);
+	return (<textarea id={'chat-input'} placeholder={'Enter chat here'} onChange={props.onChange} onKeyUp={props.onKeyUp} value={props.value}></textarea>);
 }
 
 const SendButton = (props) => {
