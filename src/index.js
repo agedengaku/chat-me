11806c5800f2306c6157f6f50b659a5d391d81d2
JM Taylor <jmotaylor2@gmail.com>
2018-12-03 19:41:11 +0900

    init commit and basic structure complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import './queries.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Wrapper extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			chatInput1: "",
+			chatOutput1: "Hi"
+		};
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleSend = this.handleSend.bind(this);
+	}
+	handleOnChange(event) {
+		this.setState({chatInput1: event.target.value});
+	}
+	handleSend() {
+		this.setState({chatOutput1: this.state.chatInput1});
+		this.setState({chatIinput1: ""});
+	}
+	render() {
+		return (<div id='wrapper'><ChatWindow value={this.state.chatOutput1} /><ChatInput onChange={this.handleOnChange} value={this.state.chatInput1} /><SendButton onClick={this.handleSend} /></div>);
+	}
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ChatWindow = (props) => {
+	const createMarkup = () => { return {__html: props.value}; }	
+	return (<div id={'chat-window'} dangerouslySetInnerHTML={createMarkup()}></div>);
+}
+
+const ChatInput = (props) => {
+	return (<textarea id={'chat-input'} placeholder={'Enter chat here'} onChange={props.onChange} value={props.value}></textarea>);
+}
+
+const SendButton = (props) => {
+	return (<button id={'send-btn'} onClick={props.onClick}>SEND</button>)
+}
+
+ReactDOM.render(<Wrapper />, document.getElementById('root'));
